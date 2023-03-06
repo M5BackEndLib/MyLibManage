06788a5198c0611ad156171c805f572c3cc06a2f
Matheus Serruya <matheus.serruya07@gmail.com>
2023-03-06 20:07:12 -0300

    Add AbstractUser User model and fields.

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    email = models.CharField(max_length=150, unique=True, null=False)
+    phone = models.CharField(max_length=50)
+    avatar_url = models.CharField(max_length=255)
+    is_blocked = models.BooleanField(default=False)
+    is_employee = models.BooleanField(default=False)
