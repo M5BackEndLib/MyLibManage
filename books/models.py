24d29538f9172aef918696c8c8658231864ad1df
Alexson Pereira <alexspnpe@gmail.com>
2023-03-06 20:07:59 -0300

    feat: add models copies and books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    author = models.CharField(max_length=50)
+    cover = models.CharField(max_length=255)
+    synopsis = models.CharField(max_length=155)
+    status = models.BooleanField()
+    follows = models.ManyToManyField("Users.user", related_name="follow_books")
