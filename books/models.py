6267f9e6ee2167cbbbdec0ec749a5affd6fced64
Alexson Pereira <alexspnpe@gmail.com>
2023-03-06 20:12:50 -0300

    feat: add uuid in models Copy and Books

--- books/models.py
+++ books/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Book(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     title = models.CharField(max_length=30, unique=True)
     author = models.CharField(max_length=50)
     cover = models.CharField(max_length=255)
