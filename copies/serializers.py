06ac089cd2e9d6f0699849259c9a2ff6b49e17fe
Bravineugenio <101228018+Bravineugenio@users.noreply.github.com>
2023-03-06 21:46:26 -0300

    feat: done some serializers

---
+++ copies/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from .models import Copy
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Copy
+        fields = ["id","added_in", "book"]
