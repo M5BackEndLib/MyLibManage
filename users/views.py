865be038e073562d3c9980deb8ec0cc3d930fd20
Bravineugenio <101228018+Bravineugenio@users.noreply.github.com>
2023-03-07 20:45:40 -0300

    fix: fixing serializers and creating views

--- users/views.py
+++ users/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .serializers import Token
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from .serializers import UserSerializer
+from .models import User
+
+class UserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 
 class LoginView(TokenObtainPairView):
