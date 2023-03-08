0bbc3fd0fd34de59c2a788a8fe71385c45ea1bb4
Alexson Pereira <alexspnpe@gmail.com>
2023-03-08 12:38:18 -0300

    fix: error users/login(create user and return token)

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ import uuid
 class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     email = models.CharField(max_length=150, unique=True, null=False)
-    phone = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50, unique=True)
     avatar_url = models.CharField(max_length=255)
     is_blocked = models.BooleanField(default=False)
     is_employee = models.BooleanField(default=False)
