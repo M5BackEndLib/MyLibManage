6f422edb3fe5b7c4f87120053868dff37890099c
Alexson Pereira <alexspnpe@gmail.com>
2023-03-08 10:57:54 -0300

    refact: refact models copies and loans

--- copies/models.py
+++ copies/models.py
@@ -7,21 +7,25 @@ class Copy(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     added_in = models.DateField(auto_now=True)
     book = models.ForeignKey(
-        "Books.book", on_delete=models.CASCADE, related_name="copies"
+        "books.Book", on_delete=models.CASCADE, related_name="copies"
     )
-    loans = models.ManyToManyField("User", related_name="users")
+    loans = models.ManyToManyField(
+        "users.User", through="CopyLoan", related_name="loans_copies"
+    )
 
 
-class Loan(models.Model):
+class CopyLoan(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     loan_in = models.DateTimeField(default=timezone.now)
     returned = models.BooleanField(default=False)
     returned_in = models.DateTimeField()
     user = models.ForeignKey(
         "users.User",
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="user_loan_copies",
     )
     copy = models.ForeignKey(
-        "copies.Copy",
-        on_delete=models.CASCADE
+        Copy,
+        on_delete=models.CASCADE,
+        related_name="copies_loan",
     )
